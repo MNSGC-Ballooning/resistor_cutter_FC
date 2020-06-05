87fce3c10e7f214f0096dc3156bfc1e87a22bd3d
SteeleSomething <50966867+SteeleSomething@users.noreply.github.com>
2020-06-05 14:53:33 -0500

    Add files via upload

--- main_computer/comms.ino
+++ main_computer/comms.ino
@@ -1,27 +1,36 @@
-void checkComms(char cutter){
+bool checkComms(char cutter){
    switch (cutter) {
       case 'A': // if cutter A selected
-        byte input[21] = {0}; // initialize data collection byte array
-        if(blueSerialA.available()>21) // collects 21 bytes, total number sent (change to 23 if using checksum)
+        byte input[23] = {0}; // initialize data collection byte array
+        uint16_t checksumCheck = 0;
+        if(blueSerialA.available()>22) // collects 20 bytes
         {
-          for(int i=0; i<21; i++)
+          for(int i=0; i<23; i++)
           {
             input[i] = blueSerialA.read(); // saves each byte into byte array input
+            if (i<20) checksumCheck += input[i];
           }
-          memcpy(&dataPacketA,&input,21); // copies input onto struct dataPacket1, a readable format
+          memcpy(&dataPacketA,&input,23); // copies input onto struct dataPacket1, a readable format
+          if(dataPacketA.checksum != checksumCheck) return false;
+          if((dataPacketA.startByte != 0x42) || (dataPacketA.stopByte != 0x53)) return false; // what to do if these don't match??
+          
           break;
         }
           
       case 'B': // if cutter B selected
-        byte input[21] = {0}; // initialize data collection byte array
-        if(blueSerialB.available()>21) // collects 21 bytes, total number sent (change to 23 if using checksum)
+        byte input[23] = {0}; // initialize data collection byte array
+        uint16_t checksumCheck = 0;
+        if(blueSerialB.available()>22) // collects 20 bytes
         {
-          for(int i=0; i<21; i++)
+          for(int i=0; i<23; i++)
           {
             input[i] = blueSerialB.read(); // saves each byte into byte array input
+            if (i<20) checksumCheck += input[i];
           }
-          memcpy(&dataPacketB,&input,21); // copies input onto struct dataPacket1, a readable format
+          memcpy(&dataPacketB,&input,23); // copies input onto struct dataPacket1, a readable format
+          if(dataPacketB.checksum != checksumCheck) return false;
+          if((dataPacketB.startByte != 0x42) || (dataPacketB.stopByte != 0x53)) return false; // what to do if these don't match??
+          
           break;
-        }
    }
 }
